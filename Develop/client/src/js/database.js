8a04137b5dc3c0f417ca24ded63e632dbceb0568
Emily Hill <emhillmed2021@gmail.com>
2022-12-22 23:23:21 -0600

    moved logic for database.js and install.js from old repo

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,21 +1,44 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('jate_db', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
+      if (db.objectStoreNames.contains('jate_db')) {
         console.log('jate database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      db.createObjectStore('jate_db', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jateDb = await openDB('jate_db', 1);
+
+  const tx = jateDb.transaction('jate_db', 'readwrite');
+
+  const store = tx.objectStore('jate_db');
+
+  const request = store.put({ id: 1, value: content});
+
+  const result = await request; 
+  console.log('Content saved to database', result.value); 
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+
+  const jateDb = await openDB('jate_db', 1);
+
+  const tx = jateDb.transaction('jate_db', 'readonly');
+
+  const store = tx.objectStore('jate_db');
+
+  const request = store.getAll();
+
+  const result = await request; 
+  return result.value; 
+};
 
 initdb();
